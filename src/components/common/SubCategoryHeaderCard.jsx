b26d7ff30a9b7f5bbb2b6ee70287d0161bc7f74e
tahamah <taha.iu.bd@gmail.com>
2023-04-05 10:13:26 +0600

    git ssh add

--- src/components/common/SubCategoryHeaderCard.jsx
+++ src/components/common/SubCategoryHeaderCard.jsx
@@ -4,7 +4,7 @@ import Image from '../../components/common/Image'
 const SubCategoryHeaderCard = ({ data }) => {
   const { name, img } = data
   return (
-    <div className='border flex items-center justify-center gap-2 pr-4'>
+    <div className='border cursor-pointer flex items-center justify-center gap-2 pr-4'>
       <div className='w-14'>
         <Image src={img} className='rounded-l' />
       </div>
