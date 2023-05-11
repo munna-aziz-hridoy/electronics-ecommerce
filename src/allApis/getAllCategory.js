1ebc2ceb33c9d397eae4e282a53b3c7e2da37659
tahamah <taha.iu.bd@gmail.com>
2023-05-11 22:00:05 +0600

    new category add

--- src/allApis/getAllCategory.js
+++ src/allApis/getAllCategory.js
@@ -1,5 +1,6 @@
 import { serverUrl } from '@config/index'
 import { useQuery } from '@tanstack/react-query'
+import { toast } from 'react-hot-toast'
 
 // Get All Category Data
 export const useGetAllCategory = () => {
@@ -21,4 +22,30 @@ export const useRemoveCategory = ({ id }) => {
   fetch(`${serverUrl}/api/category/${id}`, {
     method: 'DELETE',
   })
+}
+
+// ADD DATA
+export const addNewCategory = (data, refetch, setOpenModal) => {
+  console.log(data)
+  fetch(`${serverUrl}/api/category`, {
+    method: 'POST',
+    headers: {
+      // authorization: `Bearer ${getToken()}`,
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify({
+      ...data,
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json)
+      if (!json.error && !json.message) {
+        toast.success('Successfully Added ')
+        refetch()
+        setOpenModal(false)
+      } else {
+        toast.error(json.message || 'Something is wrong!')
+      }
+    })
 }
