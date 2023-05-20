55be65d3e12de1556f21a6ee22990c565a2483f3
munna-aziz-hridoy <munna.aziz.hridoy@gmail.com>
2023-05-21 01:36:31 +0600

    change env

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const serverUrl = 'http://localhost:3000'
-// export const serverUrl = process.env.SERVER_URL;
+// export const serverUrl = 'http://localhost:3000'
+export const serverUrl = process.env.SERVER_URL;
