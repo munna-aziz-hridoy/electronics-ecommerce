1fbf9c61be903a059d1f4ee7545a8014abe3f505
tahamah <taha.iu.bd@gmail.com>
2023-05-23 22:33:29 +0600

    pass icon add

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -7,6 +7,7 @@ import { Container, Menu } from "..";
 import useAuthStore from "@/store/auth";
 import { CartContext } from "@/context/cart";
 
+
 function Navbar() {
   const [openMenu, setOpenMenu] = useState(false);
 
@@ -15,22 +16,28 @@ function Navbar() {
 
   const { user } = useAuthStore();
 
+
+
   const { cart } = useContext(CartContext);
 
   return (
-    <div className="border-b">
+    <div className='border-b'>
       <Container>
-        <div className="p-2 py-4">
-          <div className="flex justify-between items-center">
+        <div className='p-2 py-4'>
+          <div className='flex justify-between items-center'>
             {/* logo */}
 
-            <div className="flex justify-start items-center gap-2">
-              <Link href="/">
-                <p className="text-4xl font-light uppercase">m&s</p>
+            <div className='flex justify-start items-center gap-2'>
+              <Link href='/'>
+                <img
+                  className='h-14 w-20 rounded object-cover'
+                  src='https://res.cloudinary.com/dqbxqqhx0/image/upload/v1684818511/electronics%20e-commerce/pnig71djilv2lsatfny9.jpg'
+                  alt=''
+                />
               </Link>
               <button
                 onClick={() => setOpenMenu(true)}
-                className="md:hidden text-gray-600"
+                className='md:hidden text-gray-600'
               >
                 <RxHamburgerMenu fontSize={20} />
               </button>
@@ -39,30 +46,30 @@ function Navbar() {
             {/* nav search */}
 
             {!isCheckoutPage && (
-              <div className="hidden sm:flex w-1/2  justify-center items-center">
+              <div className='hidden sm:flex w-1/2  justify-center items-center'>
                 <input
-                  className="bg-gray-100 w-[90%] h-10 p-1 px-3 outline-none"
-                  placeholder="Search products"
+                  className='bg-gray-100 w-[90%] h-10 p-1 px-3 outline-none'
+                  placeholder='Search products'
                 />
-                <button className="flex justify-center items-center bg-[#bbd850] p-1 h-10 w-10">
-                  <HiOutlineMagnifyingGlass fontSize={20} color="#fff" />
+                <button className='flex justify-center items-center bg-[#bbd850] p-1 h-10 w-10'>
+                  <HiOutlineMagnifyingGlass fontSize={20} color='#fff' />
                 </button>
               </div>
             )}
 
             {/* nav last */}
 
-            <div className="flex justify-end items-center gap-4">
+            <div className='flex justify-end items-center gap-4'>
               {user ? (
                 <>
-                  {" "}
-                  <Link href="/user/profile">
-                    {" "}
-                    <p className="font-medium capitalize  text-gray-900">
+                  {' '}
+                  <Link href='/user/profile'>
+                    {' '}
+                    <p className='font-medium capitalize  text-gray-900'>
                       {user?.name}
                     </p>
                   </Link>
-                  <Link href="/cart">
+                  <Link href='/cart'>
                     <span className=" relative w-8 h-6 bg-[#bbd850] flex justify-center items-center rounded border-[1px] border-gray-900 text-sm cursor-pointer before:content[''] before:absolute before:border-[1px] before:border-gray-900 before:rounded-full before:w-4 before:h-6 before:top-[-15px] before:z-[-1]">
                       {cart?.total_products || 0}
                     </span>
@@ -70,13 +77,13 @@ function Navbar() {
                 </>
               ) : (
                 <>
-                  <Link href="/auth/login">
-                    <button className="font-medium capitalize  text-gray-900">
+                  <Link href='/auth/login'>
+                    <button className='font-medium capitalize  text-gray-900'>
                       sign in
                     </button>
                   </Link>
-                  <Link href="/auth/register">
-                    <button className="font-medium capitalize  text-gray-900">
+                  <Link href='/auth/register'>
+                    <button className='font-medium capitalize  text-gray-900'>
                       sign up
                     </button>
                   </Link>
@@ -93,14 +100,14 @@ function Navbar() {
         </div>
       </Container>
       {!isCheckoutPage && (
-        <div className="border-t-[1px]  border-gray-300">
+        <div className='border-t-[1px]  border-gray-300'>
           <Container>
-            <div className="p-1 flex justify-center items-center gap-10">
-              <p className="font-medium capitalize text-gray-900 text-xs">
+            <div className='p-1 flex justify-center items-center gap-10'>
+              <p className='font-medium capitalize text-gray-900 text-xs'>
                 free delivery
               </p>
-              <p className="text-black font-light">|</p>
-              <p className="font-medium capitalize text-gray-900 text-xs">
+              <p className='text-black font-light'>|</p>
+              <p className='font-medium capitalize text-gray-900 text-xs'>
                 free returns
               </p>
             </div>
@@ -108,7 +115,7 @@ function Navbar() {
         </div>
       )}
     </div>
-  );
+  )
 }
 
 export default Navbar;
