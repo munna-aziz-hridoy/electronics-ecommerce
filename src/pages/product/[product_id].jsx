2c392728830c4a2a7643b31173fa78d319298bab
munna-aziz-hridoy <munna.aziz.hridoy@gmail.com>
2023-05-30 23:02:09 +0600

    fix variant issue

--- src/pages/product/[product_id].jsx
+++ src/pages/product/[product_id].jsx
@@ -99,6 +99,7 @@ function ProductDetails() {
                       id="green-radio"
                       type="checkbox"
                       defaultValue=""
+                      checked={selectedVariant?._id === item?._id}
                       name="colored-radio"
                       className="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                     />
