851baa763685e5083837c93fa178303e2ba33cb1
tahamah <taha.iu.bd@gmail.com>
2023-06-07 13:53:25 +0600

    order page lay out change

--- src/components/common/Admin/TR/OrderTR.jsx
+++ src/components/common/Admin/TR/OrderTR.jsx
@@ -15,15 +15,15 @@ const OrderTR = ({ order, refetch }) => {
     <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
       <th
         scope="row"
-        className="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+        className="px-6 py-2  font-medium text-gray-900 whitespace-nowrap dark:text-white"
       >
         {id}
       </th>
       <td className="px-6 py-2">{created_at?.split("T")[0]}</td>
-      <td className="px-6 py-2">${total_price}</td>
-      <td className="px-6 py-2">{total_products}</td>
-      <td className="px-6 py-2">{delivery ? "Delivered" : "In progress"}</td>
-      <td className="px-6 py-2">{paid ? "Paid" : "Unpaid"}</td>
+      <td className="px-6 text-center py-2">${total_price}</td>
+      <td className="px-6 text-center py-2">{total_products}</td>
+      <td className="px-6 text-center py-2">{delivery ? "Delivered" : "In progress"}</td>
+      <td className="px-6 text-center py-2">{paid ? "Paid" : "Unpaid"}</td>
       <td className="px-6 py-2">
         <div className="flex justify-end items-center gap-7 pr-4 text-2xl ">
           <button className="duration-300 rounded-md p-1 hover:bg-green-600 hover:text-gray-50 ">
