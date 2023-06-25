334310ee66f3965d3d06a695c9e67a174cca5904
tahamah <taha.iu.bd@gmail.com>
2023-06-25 22:18:59 +0600

    dollar remove

--- src/pages/product/[product_id].jsx
+++ src/pages/product/[product_id].jsx
@@ -69,35 +69,35 @@ function ProductDetails() {
       {isLoading ? (
         <Spinner />
       ) : data ? (
-        <div className="px-1 md:px-10">
-          <div className="flex flex-col md:flex-row justify-center items-start gap-10 mt-10 mb-16">
-            <div className=" w-full md:w-1/2">
-              <div className="w-full sm:w-[80%] md:w-[90%] lg:w-[500px]  bg-gray-300 h-[550px]">
-                <img className="w-full h-full" src={selectedImage} />
+        <div className='px-1 md:px-10'>
+          <div className='flex flex-col md:flex-row justify-center items-start gap-10 mt-10 mb-16'>
+            <div className=' w-full md:w-1/2'>
+              <div className='w-full sm:w-[80%] md:w-[90%] lg:w-[500px]  bg-gray-300 h-[550px]'>
+                <img className='w-full h-full' src={selectedImage} />
               </div>
-              <div className="flex flex-wrap gap-3 h-28 max-w-full w-full  my-4">
+              <div className='flex flex-wrap gap-3 h-28 max-w-full w-full  my-4'>
                 {imagesArr?.map((item) => (
                   <img
                     onClick={() => setSelectedImage(item)}
-                    className="h-28 w-28 cursor-pointer border-2 border-gray-300"
+                    className='h-28 w-28 cursor-pointer border-2 border-gray-300'
                     key={item}
                     src={item}
                   />
                 ))}
               </div>
             </div>
-            <div className="w-full md:w-1/2  p-3">
-              <h2 className="text-3xl font-bold text-gray-700 capitalize">
+            <div className='w-full md:w-1/2  p-3'>
+              <h2 className='text-3xl font-bold text-gray-700 capitalize'>
                 {data?.name}
               </h2>
-              <p className="text-lg font-semibold text-green-700 mt-1">
-                Price: ${data?.price}
+              <p className='text-lg font-semibold text-green-700 mt-1'>
+                Price: £{data?.price}
               </p>
-              <p className="text-sm text-gray-500 pr-3 my-3">
+              <p className='text-sm text-gray-500 pr-3 my-3'>
                 {data?.short_description}
               </p>
 
-              <div className="flex flex-col gap-3 flex-wrap mt-8">
+              {/* <div className="flex flex-col gap-3 flex-wrap mt-8">
                 {data?.extras?.map((item, i) => {
                   const current_variant = selectedVariant?.find(
                     (v) => v?._id === item?._id
@@ -139,18 +139,18 @@ function ProductDetails() {
                     </div>
                   );
                 })}
-              </div>
+              </div> */}
 
-              <div className="flex justify-center items-center gap-5 mt-8">
+              <div className='flex justify-center items-center gap-5 mt-8'>
                 <button
                   onClick={handleAddToCart}
-                  className="flex justify-center items-center text-center w-[90%] h-14 my-5"
+                  className='flex justify-center items-center text-center w-[90%] h-14 my-5'
                 >
-                  <span className="w-full h-full p-2 bg-[#BDD755] text-gray-800 hover:bg-white text-center flex justify-center items-center font-semibold border-2 border-white hover:border-[#BDD755] duration-150">
+                  <span className='w-full h-full p-2 bg-[#BDD755] text-gray-800 hover:bg-white text-center flex justify-center items-center font-semibold border-2 border-white hover:border-[#BDD755] duration-150'>
                     Add to cart
                   </span>
                 </button>
-                <button className="w-14 h-14 flex justify-center items-center border-2 border-[#BDD755] text-gray-500 hover:text-gray-700 rounded-sm hover:bg-[#BDD755] duration-150">
+                <button className='w-14 h-14 flex justify-center items-center border-2 border-[#BDD755] text-gray-500 hover:text-gray-700 rounded-sm hover:bg-[#BDD755] duration-150'>
                   <AiOutlineHeart fontSize={28} />
                 </button>
               </div>
@@ -159,26 +159,26 @@ function ProductDetails() {
 
           {/* part second */}
 
-          <div className="my-14 w-full  bg-white p-2 md:p-8 border-2 border-gray-100">
-            <h2 className="text-2xl font-semibold text-gray-700 my-5">
+          <div className='my-14 w-full  bg-white p-2 md:p-8 border-2 border-gray-100'>
+            <h2 className='text-2xl font-semibold text-gray-700 my-5'>
               Description:
             </h2>
             <div
               dangerouslySetInnerHTML={{ __html: data?.description }}
-              className="text-sm text-gray-500 pr-4 "
+              className='text-sm text-gray-500 pr-4 '
             ></div>
           </div>
 
           {/* related products */}
 
-          <div className="my-14">
-            <h2 className="text-center text-2xl font-semibold text-gray-700 mt-10 mb-5">
+          <div className='my-14'>
+            <h2 className='text-center text-2xl font-semibold text-gray-700 mt-10 mb-5'>
               Related Products
             </h2>
 
-            <div className="flex items-center justify-center gap-12 flex-wrap">
+            <div className='flex items-center justify-center gap-12 flex-wrap'>
               {products.slice(0, 3).map((item) => (
-                <div className="max-w-[280px]">
+                <div className='max-w-[280px]'>
                   <ProductCard product={item} sub />
                 </div>
               ))}
@@ -189,9 +189,9 @@ function ProductDetails() {
         <p>No data Found</p>
       )}
 
-      <SignMeUp class_name={"my-16"} />
+      <SignMeUp class_name={'my-16'} />
     </Container>
-  );
+  )
 }
 
 export default ProductDetails;
