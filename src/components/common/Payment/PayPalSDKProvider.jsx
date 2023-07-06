9dc0a5f24d09c7dbdebcd84aefb853b2056e0b70
tahamah <taha.iu.bd@gmail.com>
2023-07-06 15:24:17 +0600

    paypal

--- src/components/common/Payment/PayPalSDKProvider.jsx
+++ src/components/common/Payment/PayPalSDKProvider.jsx
@@ -1,11 +1,9 @@
 import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 
-
-
- const PayPalSDKProvider = ({ children }) => {
+const PayPalSDKProvider = ({ children }) => {
   const clientId =
     'Afg0xZRAH-ud4duvymCZS7lCZ00Qi3erkP3pUt9mbTcPOsgMqJAXWCSZYV5r1TNI_tJ7rYH9zEQ9uB9x'
- // Replace with your PayPal client ID
+  // Replace with your PayPal client ID
 
   return (
     <PayPalScriptProvider
@@ -14,6 +12,6 @@ import { PayPalScriptProvider } from '@paypal/react-paypal-js'
       {children}
     </PayPalScriptProvider>
   )
- }
+}
 
- export default PayPalSDKProvider
+export default PayPalSDKProvider
