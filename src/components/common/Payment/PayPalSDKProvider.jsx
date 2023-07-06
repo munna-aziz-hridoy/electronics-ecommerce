147cc7931ad6f05625e2dc1ae3c1a5ffb8050688
tahamah <taha.iu.bd@gmail.com>
2023-07-06 15:17:24 +0600

    paypal

---
+++ src/components/common/Payment/PayPalSDKProvider.jsx
@@ -0,0 +1,17 @@
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
+
+
+
+ const PayPalSDKProvider = ({ children }) => {
+  const clientId =
+    'Afg0xZRAH-ud4duvymCZS7lCZ00Qi3erkP3pUt9mbTcPOsgMqJAXWCSZYV5r1TNI_tJ7rYH9zEQ9uB9x'
+ // Replace with your PayPal client ID
+
+  return (
+    <PayPalScriptProvider options={{ 'client-id': clientId }}>
+      {children}
+    </PayPalScriptProvider>
+  )
+ }
+
+ export default PayPalSDKProvider
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,21 @@
-import { Toaster } from "react-hot-toast";
+import { Toaster } from 'react-hot-toast'
 
-import Layout from "@/components/layouts";
-import CartProvider from "@/context/cart";
+import Layout from '@/components/layouts'
+import CartProvider from '@/context/cart'
 
-import "@/styles/globals.css";
-import ProductProvider from "@/context/product";
+import '@/styles/globals.css'
+import ProductProvider from '@/context/product'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 
 export default function App({ Component, pageProps }) {
   return (
     <ProductProvider>
-    <CartProvider>
-      <Layout>
-        <Component {...pageProps} />
-        <Toaster />
-      </Layout>
-    </CartProvider>
+      <CartProvider>
+        <Layout>
+          <Component {...pageProps} />
+          <Toaster />
+        </Layout>
+      </CartProvider>
     </ProductProvider>
-  );
+  )
 }
