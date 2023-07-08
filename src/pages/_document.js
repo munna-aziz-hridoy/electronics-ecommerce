a150c423946f818e62783da2794496a73c688c14
tahamah <taha.iu.bd@gmail.com>
2023-07-08 10:20:35 +0600

    paypal

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head></Head>
+    <Html lang='en'>
+      <Head>
+        <script src='https://www.paypal.com/sdk/js?client-id=Afg0xZRAH-ud4duvymCZS7lCZ00Qi3erkP3pUt9mbTcPOsgMqJAXWCSZYV5r1TNI_tJ7rYH9zEQ9uB9x'></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
